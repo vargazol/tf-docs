a8531ca8a5029a4844f2ae7a905be75b9fbb5999
Zoltán Varga <1575861+vargazol@users.noreply.github.com>
2024-11-04 12:14:06 +0100

    Make script faster
    - downloads are multithreaded
    - downloading and renaming are separated

--- tf-docs.sh
+++ tf-docs.sh
@@ -1,32 +1,34 @@
 #!/bin/bash -e
 
 tfproviders=(
-    "azuread:60958"
-    "azurerm:61820"
-    "helm:61379"
-    "http:59770"
-    "kubectl:61545" # alekc/kubectl
-    "kubernetes:61209"
-    "local:59855"
-    "null:59862"
-    "random:59857"
-    "tls:59867"
-    )
+    azuread:60958
+    azurerm:61820
+    helm:61379
+    http:59770
+    kubectl:61545 # alekc/kubectl
+    kubernetes:61209
+    local:59855
+    null:59862
+    random:59857
+    tls:59867
+)
 
 baseUrl="https://registry.terraform.io/v2"
 mkdir -p ./terraform_azure_docs
 
-for item in "${tfproviders[@]}"; do
-    providerShortName="${item%%:*}"
-    providerId="${item#*:}"
-    providerUrl="${baseUrl}/provider-versions/${providerId}?include=provider-docs"
+download_docs() {
+    local providerShortName="$1"
+    local providerId="$2"
 
+    providerUrl="${baseUrl}/provider-versions/${providerId}?include=provider-docs"
     wget -q -O - "$providerUrl" | jq -r > ./terraform_azure_docs/"${providerShortName}"
+
     if [[ "kubectl" == "$providerShortName" ]]; then
         providerFullName="$providerShortName"
     else
         providerFullName="$(jq -r '.data.attributes.description' ./terraform_azure_docs/"${providerShortName}")"
     fi
+
     providerVersion="$(jq -r '.data.attributes.version' ./terraform_azure_docs/"${providerShortName}")"
     documentPaths="$(jq -r '.included[].links.self' ./terraform_azure_docs/"${providerShortName}" | sort)"
     totalDocumentCount="$(echo "$documentPaths" | wc -l)"
@@ -36,23 +38,40 @@ for item in "${tfproviders[@]}"; do
     targetDir="./terraform_azure_docs/${providerFullName}_${providerVersion}"
     mkdir -p "$targetDir"
     fetching=1
-    echo ""
 
     while [[ $currentPage -le $lastPage ]]; do
         echo "Fetching documentation of ${providerFullName} v${providerVersion}, ${fetching}/${totalDocumentCount}"
-        wget -q -O - "${baseUrl}/provider-docs/${currentPage}" | jq '.data.attributes' > "$targetDir"/temp
-        category="$(jq -r '.category' "$targetDir"/temp)"
-        title="$(jq -r '.title' "$targetDir"/temp | sed 's/:/ -/g')"
-        jq -r '.content' "$targetDir"/temp > "$targetDir"/output
+        wget -q -O - "${baseUrl}/provider-docs/${currentPage}" | jq '.data.attributes' > "$targetDir"/"${currentPage}.tmp" &
+        
+        fetching="$((fetching + 1))"
+        currentPage="$((currentPage + 1))"
+    done
+    wait
+
+    for tmpFile in "$targetDir"/*.tmp; do
+        category="$(jq -r '.category' "$tmpFile")"
+        title="$(jq -r '.title' "$tmpFile" | sed 's/:/ -/g')"
+        jq -r '.content' "$tmpFile" > "$targetDir"/output
         sed -i -e 's/"---//g' -e 's/\\n"//g' -e 's/\\"/\"/g' -e 's/\\n/\n/g' -e 's/```hcl/\n```hcl/g' -e 's/```##/\n```##\n/g' "$targetDir"/output
         mv "$targetDir"/output "$targetDir"/"${category}_${title}".md
+        
         if [[ "$(grep -c "$providerShortName" "$targetDir"/"${category}_${title}".md)" -eq 0 ]]; then
             echo "Downloaded document (\"${category}_${title}.md\") does not belong in the \"${providerFullName}\" provider."
             echo "Page url: ${baseUrl}/provider-docs/${currentPage}."
             rm "$targetDir"/"${category}_${title}".md
         fi
-        rm "$targetDir"/temp
-        fetching="$((fetching + 1))"
-        currentPage="$((currentPage + 1))"
+        rm "$tmpFile"
     done
+}
+
+export -f download_docs
+export baseUrl
+
+for item in "${tfproviders[@]}"; do
+    providerShortName="${item%%:*}"
+    providerId="${item#*:}"
+    download_docs "$providerShortName" "$providerId" &
 done
+
+wait
+echo "All downloads completed."
